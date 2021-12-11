aea0be12b0770cd630920dbf8d1154622e3d1e23
buivantan01 <buivantan1234566@gmail.com>
2021-12-11 09:35:35 +0700

    Design something frontend

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,122 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-      <h1>dome</h1>
+      <div className="navbar app">Navbar App</div>
+      <div className="navbar board">navbar heading</div>
+      <div className="board-columns">
+        <div className="column">
+          <header>header</header>
+          <ul>
+            <li>
+              <img src="https://avatars.githubusercontent.com/u/67332180?v=4" alt="vantan" />
+              title: vantan
+            </li>
+            <li>Live serrver lesson</li>
+            <li>Live serrver lesson</li>
+            <li>Live serrver lesson</li>
+            <li>Live serrver lesson</li>
+            <li>Live serrver lesson</li>
+            <li>Live serrver lesson</li>
+          </ul>
+          <footer>Add commit</footer>
+        </div>
+        <div className="column">
+          <header>header</header>
+          <ul>
+            <li>
+              <img src="https://avatars.githubusercontent.com/u/67332180?v=4" alt="vantan" />
+              title: vantan
+            </li>
+            <li>Live serrver lesson</li>
+            <li>Live serrver lesson</li>
+            <li>Live serrver lesson</li>
+            <li>Live serrver lesson</li>
+            <li>Live serrver lesson</li>
+            <li>Live serrver lesson</li>
+          </ul>
+          <footer>Add commit</footer>
+        </div>
+        <div className="column">
+          <header>header</header>
+          <ul>
+            <li>
+              <img src="https://avatars.githubusercontent.com/u/67332180?v=4" alt="vantan" />
+              title: vantan
+            </li>
+            <li>Live serrver lesson</li>
+            <li>Live serrver lesson</li>
+            <li>Live serrver lesson</li>
+            <li>Live serrver lesson</li>
+            <li>Live serrver lesson</li>
+            <li>Live serrver lesson</li>
+          </ul>
+          <footer>Add commit</footer>
+        </div>
+        <div className="column">
+          <header>header</header>
+          <ul>
+            <li>
+              <img src="https://avatars.githubusercontent.com/u/67332180?v=4" alt="vantan" />
+              title: vantan
+            </li>
+            <li>Live serrver lesson</li>
+            <li>Live serrver lesson</li>
+            <li>Live serrver lesson</li>
+            <li>Live serrver lesson</li>
+            <li>Live serrver lesson</li>
+            <li>Live serrver lesson</li>
+          </ul>
+          <footer>Add commit</footer>
+        </div>
+        <div className="column">
+          <header>header</header>
+          <ul>
+            <li>
+              <img src="https://avatars.githubusercontent.com/u/67332180?v=4" alt="vantan" />
+              title: vantan
+            </li>
+            <li>Live serrver lesson</li>
+            <li>Live serrver lesson</li>
+            <li>Live serrver lesson</li>
+            <li>Live serrver lesson</li>
+            <li>Live serrver lesson</li>
+            <li>Live serrver lesson</li>
+          </ul>
+          <footer>Add commit</footer>
+        </div>
+        <div className="column">
+          <header>header</header>
+          <ul>
+            <li>
+              <img src="https://avatars.githubusercontent.com/u/67332180?v=4" alt="vantan" />
+              title: vantan
+            </li>
+            <li>Live serrver lesson</li>
+            <li>Live serrver lesson</li>
+            <li>Live serrver lesson</li>
+            <li>Live serrver lesson</li>
+            <li>Live serrver lesson</li>
+            <li>Live serrver lesson</li>
+          </ul>
+          <footer>Add commit</footer>
+        </div>
+        <div className="column">
+          <header>header</header>
+          <ul>
+            <li>
+              <img src="https://avatars.githubusercontent.com/u/67332180?v=4" alt="vantan" />
+              title: vantan
+            </li>
+            <li>Live serrver lesson</li>
+            <li>Live serrver lesson</li>
+            <li>Live serrver lesson</li>
+            <li>Live serrver lesson</li>
+            <li>Live serrver lesson</li>
+            <li>Live serrver lesson</li>
+          </ul>
+          <footer>Add commit</footer>
+        </div>
+      </div>
     </div>
   );
 }
